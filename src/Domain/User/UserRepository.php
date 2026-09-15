ff83c7555aaaae94e07a1079222ffcf0e5bd35b1
OlegTDev <_alexeevich_@list.ru>
2026-09-15 16:55:19 +0500

    feat: исправлен интерфейс репозитория UserRepository (удалены ненужные функции и добавлена только 1 нужная)

--- src/Domain/User/UserRepository.php
+++ src/Domain/User/UserRepository.php
@@ -6,15 +6,5 @@ namespace App\Domain\User;
 
 interface UserRepository
 {
-    /**
-     * @return User[]
-     */
-    public function findAll(): array;
-
-    /**
-     * @param int $id
-     * @return User
-     * @throws UserNotFoundException
-     */
-    public function findUserOfId(int $id): User;
+    public function findBySamaccountName(string $samaccountName): User;
 }
